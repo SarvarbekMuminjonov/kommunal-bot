69a1750db4fbac1c36ecfa904c9b08e79a0c3c31
IbrohimMirxoshimov <imirxoshimov@gmail.com>
2021-02-12 12:15:35 +0500

    first commit

---
+++ app/services/request/exemple.js
@@ -0,0 +1,12 @@
+const getDataUser = require("./getDataUser");
+const creditials = {
+    personal_account: 2276071,
+    service_id: 3,
+    region_id: 27,
+    sub_region_id: 237
+}
+getDataUser(creditials).then(res => {
+    console.log(res.message);
+}).catch(err => {
+    console.log(err);
+})
---
+++ app/services/request/getDataUser.js
@@ -0,0 +1,81 @@
+const request = require("request").defaults({ jar: true })
+
+module.exports = async function getDataUser(creditials) {
+    const { personal_account, service_id, region_id, sub_region_id } = creditials
+    if (!personal_account || !service_id || !region_id || !sub_region_id)
+        throw new Error("Cannot be empty")
+
+    const firstResponse = await getFirstResponse()
+    return await getMainResponse(firstResponse, creditials)
+}
+
+function getFirstResponse() {
+    return new Promise((resolve, reject) => {
+        request({
+            method: "GET",
+            url: "http://ek.uz/ru/cabinet/payment-public/balance?id=3"
+        }, (error, response, body) => {
+            if (error) {
+                reject({
+                    ok: false,
+                    message: error
+                })
+            } else {
+                resolve(response)
+            }
+
+        })
+    })
+}
+
+function getMainResponse(firstResponse, creditials) {
+    const token = parseToken(firstResponse.body)
+
+    return new Promise((resolve, reject) => {
+
+        request({
+            method: "POST",
+            host: "ek.uz",
+            url: "http://ek.uz/ru/cabinet/payment-public/balance?id=3",
+            form: {
+                _csrf: token,
+                'Balance[personal_account]': creditials.personal_account,
+                'Balance[region_id]': creditials.region_id,
+                'Balance[service_id]': creditials.service_id,
+                'Balance[sub_region_id]': creditials.sub_region_id
+            },
+        }, (error, response, body) => {
+            if (error) {
+                reject({
+                    ok: false,
+                    message: error
+                })
+            } else {
+                if (body.search("Ошибка системы проверки баланса") > 0) {
+                    reject({
+                        ok: false,
+                        message: "NOT FOUND"
+                    })
+                } else if (body.search("table table-striped") > 0) {
+                    resolve({
+                        ok: true,
+                        message: "SUCCESS",
+                        body,
+                    })
+                } else {
+                    reject({
+                        ok: false,
+                        message: "ERROR"
+                    })
+                }
+            }
+        })
+    })
+
+}
+
+function parseToken(body) {
+    const start = body.search("csrf-token") + 21
+    const end = body.search("==") + 2
+    return body.slice(start, end)
+}
