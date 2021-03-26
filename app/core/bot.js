6c21969c29276c78807cf46caa06e8732ddb445b
SarvarbekMuminjonov1 <smomimjonov20@gmail.com>
2021-03-26 16:33:23 +0500

    fix

--- app/core/bot.js
+++ app/core/bot.js
@@ -1,26 +1,29 @@
 const { Telegraf, Scenes, session } = require("telegraf")
-const { TOKEN ,URL , PORT} = require("../config/index.js")
+const { TOKEN ,URL , PORT,DEV_ID} = require("../config/index.js")
 const { Stage } = Scenes
 const express = require("express")
 const app = express()
-
+console.log("TOKEN", TOKEN);
 const bot = new Telegraf(TOKEN)
 const scenes = require("../scenes")
 const stage = new Stage(scenes)
 
-bot.telegram.setWebhook(`${URL}/bot${TOKEN}`)
 app.use(bot.webhookCallback(`/bot${TOKEN}`))
 
 bot
-  .use(session())
-  .use(stage.middleware())
+.use(session())
+.use(stage.middleware())
   .start((ctx) => ctx.scene.enter("lang"))
   .on("text", (ctx) => ctx.scene.enter("lang"))
 // .launch()
 // .then((res) => {
 // 	console.log("Bot started")
-// 	//bot.telegram.sendMessage(DEV_ID, "bot started")
-// })
+	//bot.telegram.sendMessage(DEV_ID, "bot started")
+  // })
+  bot.telegram.setWebhook(`${URL}/bot${TOKEN}`).then(res => {
+    
+    bot.telegram.sendMessage(DEV_ID, "bot started")
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
