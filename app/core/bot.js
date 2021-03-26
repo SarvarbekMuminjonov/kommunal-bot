f7a4315af2804182f13c5b2a8e1aefd5c87e0cb2
SarvarbekMuminjonov1 <smomimjonov20@gmail.com>
2021-03-26 22:19:08 +0500

    fix

--- app/core/bot.js
+++ app/core/bot.js
@@ -1,33 +1,30 @@
-const { Telegraf, Scenes, session } = require("telegraf")
-const { TOKEN ,URL , PORT,DEV_ID} = require("../config/index.js")
-const { Stage } = Scenes
-const express = require("express")
-const app = express()
+const { Telegraf, Scenes, session } = require("telegraf");
+const { TOKEN, URL, PORT, DEV_ID } = require("../config/index.js");
+const { Stage } = Scenes;
+const express = require("express");
+const app = express();
 console.log("TOKEN", TOKEN);
-console.log("port",PORT)
-const bot = new Telegraf(TOKEN)
-const scenes = require("../scenes")
-const stage = new Stage(scenes)
+console.log("port", PORT);
+const bot = new Telegraf(TOKEN);
+const scenes = require("../scenes");
+const stage = new Stage(scenes);
 
-bot.telegram.setWebhook(`${URL}/bot${TOKEN}`).then(res => {
-    
-  bot.telegram.sendMessage(DEV_ID, "bot started")
-})
-
-app.use(bot.webhookCallback(`/bot${TOKEN}`))
+app.use(bot.webhookCallback(`/bot${TOKEN}`));
 
 bot
-.use(session())
-.use(stage.middleware())
+  .use(session())
+  .use(stage.middleware())
   .start((ctx) => ctx.scene.enter("lang"))
-  .on("text", (ctx) => ctx.scene.enter("lang"))
+  .on("text", (ctx) => ctx.scene.enter("lang"));
 // .launch()
 // .then((res) => {
 // 	console.log("Bot started")
-	//bot.telegram.sendMessage(DEV_ID, "bot started")
-  // })
- 
+//bot.telegram.sendMessage(DEV_ID, "bot started")
+// })
+bot.telegram.setWebhook(`${URL}/bot${TOKEN}`).then((res) => {
+  bot.telegram.sendMessage(DEV_ID, "bot started");
+});
 
 app.listen(PORT || 8000, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
